24024e1dbd39035aef4735c1fd7154eea894e289
Seme87 <massimomonteleone@hotmail.com>
2022-11-16 16:19:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        emails:[]
+      }
+    },
+    methods: {
+
+    },
+    created(){
+        
+        for( let i= 1; i <= 10; i++ ){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) =>{
+                
+                this.emails.push(response.data.response);
+            })
+        }
+    }
+  }).mount('#app')
